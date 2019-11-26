7e9cbc0197e86103ea24f1cd5a099d93754460f3
Mohamed MAJDEB <mohamedmajdeb@gmail.com>
2019-11-26 17:56:43 +0100

    comment code

--- Test/Controllers/NamesController.cs
+++ Test/Controllers/NamesController.cs
@@ -12,12 +12,20 @@ namespace Test.Controllers
         private IMemoryCache _cache;
         private readonly string _cashKey;
 
+        /// <summary>
+        /// Constructer
+        /// </summary>
+        /// <param name="memoryCache">inject MemoryCache</param>
         public NamesController(IMemoryCache memoryCache)
         {
             _cache = memoryCache;
             _cashKey = "Names";
         }
 
+        /// <summary>
+        /// Index page, show the names list
+        /// </summary>
+        /// <returns>index view, list of names</returns>
         public IActionResult Index()
         {
             if (!_cache.TryGetValue(_cashKey, out listName))
@@ -27,11 +35,20 @@ namespace Test.Controllers
             return View(listName);
         }
 
+        /// <summary>
+        /// Show the form to create new Name
+        /// </summary>
+        /// <returns>create view</returns>
         public ActionResult Create()
         {
             return View();
         }
 
+        /// <summary>
+        /// Create new Name and add it to list
+        /// </summary>
+        /// <param name="model">name object</param>
+        /// <returns>redirect to Index if all done, if not: return the same view and shows errors</returns>
         [HttpPost]
         public ActionResult Create(Names model)
         {
@@ -41,6 +58,7 @@ namespace Test.Controllers
                 {
                     listName = new List<Names>();
                 }
+
                 listName.Add(model);
                 _cache.Set(_cashKey, listName);
 
@@ -52,6 +70,12 @@ namespace Test.Controllers
             }
         }
 
+        /// <summary>
+        /// Filter list of item
+        /// </summary>
+        /// <param name="IdMin">min value of filter</param>
+        /// <param name="IdMax">max value of filter</param>
+        /// <returns>A partial view which content filred list</returns>
         [HttpPost]
         public ActionResult Filter(int IdMin, int IdMax)
         {
@@ -59,7 +83,7 @@ namespace Test.Controllers
             {
                 listName = new List<Names>();
             }
-            
+
             var listNameFiltred = listName.Where(x => x.Id >= IdMin && x.Id <= IdMax || (IdMin == 0 && IdMax == 0)).ToList();
 
             return PartialView("_FilteredList", listNameFiltred);
